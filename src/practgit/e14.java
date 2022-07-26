66ee95af41b347ad3123fefbbd3acbb947e178c8
ASHISH <ASHISH@LAPTOP-HTRNMPSK>
2022-07-26 23:38:29 +0530

    add commit

---
+++ src/practgit/e14.java
@@ -0,0 +1,22 @@
+package practgit;
+
+
+public class e14 {
+	
+	// java printing statement without using main 
+	
+	public static int printstring(){
+		
+		System.out.println("hello world");
+		
+		return 0;
+		}
+	
+     static int p=printstring();
+
+
+    public static void main (String[]args)
+    {
+    	
+	}
+   }
